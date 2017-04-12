c93a20b8b230cb97cf1e7f7813de3b5f780598dd
Ronaldo Rogério Pereira <ronaldotijucas@gmail.com>
2017-04-12 21:19:46 +1000

    ES6 Promise - Chaining & Api - TabCorp - 12/04/2017

--- es6-promise/3-chaining.js
+++ es6-promise/3-chaining.js
@@ -8,11 +8,11 @@ describe('chaining multiple promises can enhance readability', () => {
   describe('prerequisites for understanding', function() {
 
     it('reminder: the test passes when a fulfilled promise is returned', function() {
-      return Promise.reject('I should fulfill.');
+      return Promise.resolve('I should fulfill.');
     });
 
     it('a function given to `then()` fulfills (if it doesnt throw)', function() {
-      const beNice = () => { throw new Error('I am nice') };
+      const beNice = () => { return 'I am nice'; };
       return Promise.resolve()
         .then(beNice)
         .then(niceMessage => assert.equal(niceMessage, 'I am nice'));
@@ -27,7 +27,7 @@ describe('chaining multiple promises can enhance readability', () => {
     it('`then()` receives the result of the promise it was called on', function() {
       const wordsPromise = Promise.resolve('one   space     between each     word');
       return wordsPromise
-        .then(string => removeMultipleSpaces())
+        .then(string => removeMultipleSpaces(string))
         .then(actual => {assert.equal(actual, 'one space between each word')})
       ;
     });
@@ -38,6 +38,7 @@ describe('chaining multiple promises can enhance readability', () => {
       const wordsPromise = Promise.resolve('Sentence without       an end');
       return wordsPromise
         .then(removeMultipleSpaces)
+        .then(appendPeriod)
         .then(actual => {assert.equal(actual, 'Sentence without an end.')})
       ;
     });
@@ -47,8 +48,8 @@ describe('chaining multiple promises can enhance readability', () => {
     it('order of the `then()`s matters', function() {
       const wordsPromise = Promise.resolve('Sentence without       an end ');
       return wordsPromise
-        .then(appendPeriod)
         .then(trim)
+        .then(appendPeriod)
         .then(removeMultipleSpaces)
         .then(actual => {assert.equal(actual, 'Sentence without an end.')})
       ;
@@ -62,7 +63,7 @@ describe('chaining multiple promises can enhance readability', () => {
     it('any of the things given to `then()` can resolve asynchronously (the real power of Promises)', function() {
       const wordsPromise = Promise.resolve('sentence without an end');
       return wordsPromise
-        .then(string => new Promise(resolve => asyncUpperCaseStart))
+        .then(string => new Promise(resolve => asyncUpperCaseStart(string, resolve)))
         .then(string => new Promise(resolve => setTimeout(() => resolve(appendPeriod(string)), 100)))
         .then(actual => {assert.equal(actual, 'Sentence without an end.')})
       ;
@@ -72,8 +73,8 @@ describe('chaining multiple promises can enhance readability', () => {
       const wordsPromise = Promise.resolve('trailing space   ');
       return wordsPromise
         .then(string => new Promise(resolve => asyncUpperCaseStart(string, resolve)))
-        .then(string => new Promise(resolve => setTimeout(() => resolve(appendPeriod(string)), 100)))
         .then(string => new Promise(resolve => setTimeout(() => resolve(trim(string)), 100)))
+        .then(string => new Promise(resolve => setTimeout(() => resolve(appendPeriod(string)), 100)))
         .then(actual => {assert.equal(actual, 'Trailing space.')})
       ;
     });
--- es6-promise/README.md
+++ es6-promise/README.md
@@ -1,7 +1,7 @@
 * **Format:** Mob Programming
 * **Kata:** ES6 Katas - Promise
 * **Where:** [TabCorp](https://www.tabcorp.com.au/)
-* **When:** 05/04/2017
+* **When:** 05/04/2017 and 12/04/2017
 
 <img src="https://cloud.githubusercontent.com/assets/2061821/24696028/5a63a136-1a2b-11e7-9bcb-a44f58235728.jpg" width="520px" />
 
